d8b7b10a347eb8fa3b4d6ecceeae47470e5a8c7e
Sneha Chaudhari <67182479+Sneha14-sc@users.noreply.github.com>
2021-02-01 23:39:17 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,17 @@
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int i=0;
+  
+	char ch='I';	 		
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+	System.out.println("Entered character "+ch+" is  Vowel"); 
+	}
+	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonant");
+	      else
+		System.out.println("Not an alphabet");		
+	}
+}
